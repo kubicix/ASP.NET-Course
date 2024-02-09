42b940dc6cd27f23acb5548ee20ab39b43a00ede
Kubilay Birer <kubilaybirer@hotmail.com>
2024-02-09 13:54:34 +0300

    learned how to work with layouts and created navbar layout with bootstrap 5.1

--- basics/Controllers/CourseController.cs
+++ basics/Controllers/CourseController.cs
@@ -15,6 +15,7 @@ namespace basics.Controllers
             course.Id=1;
             course.Title = "ASP.NET Core Course";
             course.Description = "Nice course for web development";
+            course.Image="1.jpg";
             return View(course);
         }
         // course/list
@@ -24,22 +25,26 @@ namespace basics.Controllers
                 new Course() {
                     Id =1,
                     Title="aspnet course",
-                    Description="nice course"
+                    Description="nice course",
+                    Image="1.jpg"
                 },
                 new Course() {
                     Id =2,
                     Title="php",
-                    Description="nice course"
+                    Description="nice course",
+                    Image="3.jpg"
                 },
                 new Course() {
                     Id =3,
                     Title="django course",
-                    Description="nice course"
+                    Description="nice course",
+                    Image="2.jpg"
                 },
                 new Course() {
                     Id =4,
                     Title="Javascript course",
-                    Description="nice course"
+                    Description="nice course",
+                    Image="3.jpg"
                 }
             };
             return View("CourseList",kurslar);
