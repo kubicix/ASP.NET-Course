dc46e5a20671c3c9b59ccb8d366a2dba9df736b1
Kubilay Birer <kubilaybirer@hotmail.com>
2024-02-18 14:53:45 +0300

    created product and category models and designed index page

---
+++ FormsApp/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using FormsApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FormsApp.Controllers
+{
+	public class HomeController : Controller
+	{
+	
+
+		public HomeController()
+		{
+			
+		}
+
+		public IActionResult Index(string searchString)
+		{
+			var products = Repository.Products;
+
+			if (!String.IsNullOrEmpty(searchString))
+			{
+				ViewBag.SearchString = searchString;
+				products = products.Where(p => p.Name.ToLower().Contains(searchString)).ToList();
+			}
+			return View(products);
+			
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+	}
+}
---
+++ FormsApp/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FormsApp.Models
+{
+	public class Product
+	{
+        [Display(Name="Product Id")]
+        public int ProductId { get; set; }
+		[Display(Name = "Product Name")]
+		public string? Name { get; set; }
+        public decimal Price { get; set; }
+        public string? Image { get; set; }
+        public bool? IsActive { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
+
+// 1 iphone 14 1
+// 2 iphone 15 1
