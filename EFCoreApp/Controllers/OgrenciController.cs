7ff5652c48e9d6584ec4c8b9a4c5cc33f740624f
Kubilay Birer <kubilaybirer@hotmail.com>
2024-06-15 13:36:20 +0300

    created edit page and succesfully withdrawed students to edit page with their ids

--- EFCoreApp/Controllers/OgrenciController.cs
+++ EFCoreApp/Controllers/OgrenciController.cs
@@ -29,5 +29,21 @@ namespace EFCoreApp.Controllers
 			await _context.SaveChangesAsync();
 			return RedirectToAction("Index");
 		}
+
+		public async Task<IActionResult> Edit(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
+
+			var ogr = await _context.Ogrenciler.FindAsync(id);
+			//var ogr = await _context.Ogrenciler.FirstOrDefaultAsync(ogrenci => ogrenci.OgrenciId == id);
+			if (ogr == null) 
+			{
+				return NotFound();	
+			}
+			return View(ogr);
+		}
 	}
 }
--- EFCoreApp/obj/Debug/net7.0/EFCoreApp.AssemblyInfo.cs
+++ EFCoreApp/obj/Debug/net7.0/EFCoreApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EFCoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9563731eee6fbd657893ffe79bc572423180b1d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5623e4242f7460ccd9520b9d420634ce6fb8b7d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("EFCoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EFCoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
