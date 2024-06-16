125a723da422019199fabae7c418a603e8962556
Kubilay Birer <kubilaybirer@hotmail.com>
2024-06-16 12:44:58 +0300

    edit form controller functions added and now we can edit student information through forms

--- EFCoreApp/Controllers/OgrenciController.cs
+++ EFCoreApp/Controllers/OgrenciController.cs
@@ -45,5 +45,38 @@ namespace EFCoreApp.Controllers
 			}
 			return View(ogr);
 		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Edit(int id, Ogrenci model)
+		{
+			if (id != model.OgrenciId)
+			{
+				return NotFound();
+			}
+
+			if (ModelState.IsValid)
+			{
+				try
+				{
+					_context.Update(model);
+					await _context.SaveChangesAsync();
+				}
+				catch (DbUpdateConcurrencyException)
+				{
+					if (!_context.Ogrenciler.Any(o => o.OgrenciId == model.OgrenciId))
+					{
+						return NotFound();
+					}
+					else
+					{
+						throw;
+					}
+				}
+				return RedirectToAction("Index");
+			}
+
+			return View(model);
+		}
 	}
 }
--- EFCoreApp/obj/Debug/net7.0/EFCoreApp.AssemblyInfo.cs
+++ EFCoreApp/obj/Debug/net7.0/EFCoreApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EFCoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5623e4242f7460ccd9520b9d420634ce6fb8b7d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ff5652c48e9d6584ec4c8b9a4c5cc33f740624f")]
 [assembly: System.Reflection.AssemblyProductAttribute("EFCoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EFCoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
